b33b23d65ca2853569c861a94208b6a6b75def3a
Jon <32584149+nvpraz0r@users.noreply.github.com>
2022-03-17 20:18:06 -0500

    adding buttons to panel
    general cleaning up some of the unnecessary code

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -24,8 +24,8 @@ public class GameGUI extends JFrame implements ActionListener{
     //class variables
     public int computerThrow;
     public String playerChoice;
-    JButton[] handButtons = new JButton[5];
-    JButton rockButton, paperButton, scissorButton, lizardButton, spockButton;
+    JButton[] handButtons = new JButton[7];
+    JButton rockButton, paperButton, scissorButton, lizardButton, spockButton, randoButton, exitButton;
 
     Font myFont = new Font("Apple Casual",Font.PLAIN,25);
     
@@ -49,30 +49,49 @@ public class GameGUI extends JFrame implements ActionListener{
     private void initComponents(){
         setTitle("RockPaperScissorsLizardSpock");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setSize(400, 400);
         setLayout(null);
 
+        //create button objects
         rockButton = new JButton("Rock");
         paperButton = new JButton("Paper");
         scissorButton = new JButton("Scissors");
         lizardButton = new JButton("Lizard");
         spockButton = new JButton("Spock");
+        randoButton = new JButton("Random");
+        exitButton = new JButton("Exit");
 
+        //assign values to button array
         handButtons[0] = rockButton;
         handButtons[1] = paperButton;
         handButtons[2] = scissorButton;
         handButtons[3] = lizardButton;
         handButtons[4] = spockButton;
+        handButtons[5] = randoButton;
+        handButtons[6] = exitButton;
 
+
+        //assign action listeners and fonts to the buttons
         for (int i = 0; i < handButtons.length; i++) {
             handButtons[i].addActionListener(this);
             handButtons[i].setFont(myFont);
             handButtons[i].setFocusable(false);
         }
 
+        //set up panel
+        JPanel panel = new JPanel();
+        panel.setBounds(35, 35, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(Color.GRAY);
 
+        //add buttons to the panel
+        for (int i = 0; i < handButtons.length; i++) {
+            panel.add(handButtons[i]);
+        }
 
+        add(panel);
 
+        //make the window visible
         setVisible(true);
     }
 
